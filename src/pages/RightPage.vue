ae0fbffd7f06ec479d8ea45361b613f62a480301
Dejan21 <jangelovski_dejan@yahoo.com>
2022-08-29 12:28:55 +0200

    Left side

--- src/pages/RightPage.vue
+++ src/pages/RightPage.vue
@@ -5,9 +5,12 @@
          :key="appointment.id"
          class="p-2"
          >
+         <img :src="appointment.image" 
+              class="relative right-14 top-12" />
          <span>{{appointment.content}}</span>
          <br />
          <span class="font-bold">{{appointment.pieces}}</span>
+         <img :src="appointment.img" class="relative left-44 bottom-10" />
          </li>
         </ul>
     </div>
@@ -22,21 +25,29 @@
                 id: 1,
                 content:'New clients',
                 pieces: '44',
+                image: [require("../assets/image.png")],
+                img: [require("../assets/ArrowSquareRight.png")]
             },
             {
                 id: 2,
                 content:'Appointments booked',
                 pieces: '26',
+                image: [require("../assets/image1.png")],
+                img: [require("../assets/ArrowSquareRight.png")]
             },
             {
                 id: 3,
                 content:'Appointments booked',
                 pieces: '8',
+                image: [require("../assets/image1.png")],
+                img: [require("../assets/ArrowSquareRight.png")]
             },
             {
                 id: 4,
                 content:'Appointments booked',
                 pieces: '6',
+                image: [require("../assets/image1.png")],
+                img: [require("../assets/ArrowSquareRight.png")]
             },
             ]
         }
